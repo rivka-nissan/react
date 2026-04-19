d2073a51be801ac07f68ff03703d1734a50bee45
rivka-nissan <n025713580@gmail.com>
2026-04-19 18:38:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import Post from './components/Post'
+function App() {
+  return(
+<>
+<Post></Post>
+<Post></Post>
+<Post></Post>
+</>
+  );
+  
+ 
+}
+
+export default App
---
+++ src/components/post.jsx
@@ -0,0 +1,9 @@
+function Post() {
+const name = "rivka nissan"
+const contect = "this is my first lesson in react"
+return <div>
+    <h1>hello {name}</h1>
+<h2>{contect}</h2>
+</div>
+}
+export default Post
