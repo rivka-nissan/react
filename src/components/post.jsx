f63edd52cfb917209aaf7bca5483bed76da3fe0c
rivka-nissan <n025713580@gmail.com>
2026-04-21 19:56:27 +0300

    update styles and components

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import styles from './Post.module.css'
-function Post({ name, content }) {
-return <div className={styles.conteiner}>
+
+function Post({ name, content ,onClick}) {
+const handleClick = () => {
+    console.log('i was clicked')
+
+  }
+  const[postcontect,setpostcontect] = useState(content)
+
+return <div className={styles.conteiner}onClick={onClick}>
     <h1 className={styles.name}>hello {name}</h1>
-    <h2 className={styles.contect}>{content}</h2>
+    <h2 className={styles.contect}>{postcontect}</h2>
+    <input 
+    className={styles.input}
+    placeholder='write a post...'
+    onChange={(e) => setpostcontect(e.target.value)}
+
+    type="text" />
 </div>
 
 }
